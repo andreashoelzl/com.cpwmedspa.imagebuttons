7183d60fb35d706c33193cd2e9e252c71889007a
andreashoelzl@gmail.com <andreashoelzl@gmail.com>
2025-02-01 08:14:16 -0800

    Search autocomplete - redesign templates

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-var fs = require('fs');
+var fs = require('node:fs');
 var pages = JSON.parse(fs.readFileSync('./public/javascripts/pages.json', 'utf8'));
 var buttons = JSON.parse(fs.readFileSync('./public/javascripts/buttons.json', 'utf8'));
-
-pages.forEach((page, index) => pages[index] = (page.indexOf('/') > -1)? page.split('/').pop(): '');
+ 
+pages.forEach((page, index) => pages[index] = (page.indexOf('/') > -1)? page.split('/').pop(): "");
 buttons.forEach((button, index) => buttons[index] = button.split('/').pop());
+
 buttons.push('');
 
 /* GET home page. */
@@ -14,10 +15,10 @@ router.get('/', function(req, res, next) {
   res.render('index', {title: 'CPW Vein & Aesthetic Center' });
 });
 router.get('/visitedpages', function(req, res, next) {
-  res.render('visitedpages', {title: 'Pages', pages:pages});
+  res.render('visitedpages', {title: 'Pages', anchors:pages});
 });
 router.get('/buttons', function(req, res, next) {
-  res.render('buttons', {title: 'Buttons', buttons:buttons});
+  res.render('buttons', {title: 'Buttons', anchors:buttons});
 });
 router.get('/errors', function(req, res, next) {
   res.render('errors', {title: 'Errors'});
