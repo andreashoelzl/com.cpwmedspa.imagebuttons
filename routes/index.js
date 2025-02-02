2c73057f062f9b84ab4feeb22f5e861baaae1518
andreashoelzl@gmail.com <andreashoelzl@gmail.com>
2025-02-02 04:40:40 -0800

    switch from common to modal

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,30 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+import fs from 'node:fs';
 
-var fs = require('node:fs');
-var pages = JSON.parse(fs.readFileSync('./public/javascripts/pages.json', 'utf8'));
-var buttons = JSON.parse(fs.readFileSync('./public/javascripts/buttons.json', 'utf8'));
- 
-pages.forEach((page, index) => pages[index] = (page.indexOf('/') > -1)? page.split('/').pop(): "");
+const router = express.Router();
+
+const pages = JSON.parse(fs.readFileSync('./public/javascripts/pages.json', 'utf8'));
+const buttons = JSON.parse(fs.readFileSync('./public/javascripts/buttons.json', 'utf8'));
+
+pages.forEach((page, index) => pages[index] = (page.indexOf('/') > -1) ? page.split('/').pop() : "");
 buttons.forEach((button, index) => buttons[index] = button.split('/').pop());
 
 buttons.push('');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Welcome' });
+  res.render('index', { title: 'Welcome' });
 });
 router.get('/visitedpages', function(req, res, next) {
-  res.render('visitedpages', {title: 'Pages', anchors:pages});
+  res.render('visitedpages', { title: 'Pages', anchors: pages });
 });
 router.get('/buttons', function(req, res, next) {
-  res.render('buttons', {title: 'Buttons', anchors:buttons});
+  res.render('buttons', { title: 'Buttons', anchors: buttons });
 });
 router.get('/errors', function(req, res, next) {
+  res.render('errors', { title: 'Errors' });
+});
   res.render('errors', {title: 'Errors'});
 });
-module.exports = router;
+
+export default router;
